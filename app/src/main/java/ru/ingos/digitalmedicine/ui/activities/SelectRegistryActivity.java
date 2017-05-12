dd34112938c4853f76d15134c04e4069df23ca2d
Sylar96 <sylar.96@mail.ru>
2017-05-12 16:44:41 +0300

    Переделан экран выбора способа записи к врачу

--- app/src/main/java/ru/ingos/digitalmedicine/ui/activities/SelectRegistryActivity.java
+++ app/src/main/java/ru/ingos/digitalmedicine/ui/activities/SelectRegistryActivity.java
@@ -1,40 +1,52 @@
 package ru.ingos.digitalmedicine.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import ru.ingos.digitalmedicine.R;
-import ru.ingos.digitalmedicine.ui.adapters.SelectRegistryAdapter;
 
-public class SelectRegistryActivity extends MvpAppCompatActivity {
+public class SelectRegistryActivity extends MvpAppCompatActivity implements View.OnClickListener {
 
-    @BindView(R.id.activity_select_registry_recycler_view)
-    RecyclerView rvSelectRegistry;
+    @BindView(R.id.activity_select_registry_btn_spec)
+    Button btnSelectSpec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_registry);
+        ButterKnife.bind(this);
 
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setTitle(R.string.title_select_activity);
 
-        ButterKnife.bind(this);
+        btnSelectSpec.setOnClickListener(this);
 
-        rvSelectRegistry.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        rvSelectRegistry.setAdapter(new SelectRegistryAdapter());
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.activity_select_registry_btn_spec:
+                startActivity(new Intent(this, SpecialtyActivity.class));
+                break;
+            default:
+        }
     }
 }
