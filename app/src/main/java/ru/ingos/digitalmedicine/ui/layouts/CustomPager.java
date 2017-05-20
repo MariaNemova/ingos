a8a319d56ee4116cd655b95b5c53db5596123c8d
remmintan <lwjgl@mail.ru>
2017-05-20 14:29:28 +0300

    Размер окна ClinicInfo стал универсальным Теперь почему-то нельзя скроллить. Это вопрос.

--- app/src/main/java/ru/ingos/digitalmedicine/ui/layouts/CustomPager.java
+++ app/src/main/java/ru/ingos/digitalmedicine/ui/layouts/CustomPager.java
@@ -29,26 +29,15 @@ public class CustomPager extends ViewPager {
             return;
         }
 
-//        int height = 0;
-//        mCurrentView.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-//        int h = mCurrentView.getMeasuredHeight();
-//        if (h > height) height = h>512?h:512;
-//
-//        ListView listView = (ListView)mCurrentView.findViewById(R.id.clinic_info_services_list);
-//        if(listView != null){
-//            ServiceListAdapter adapter = (ServiceListAdapter)listView.getAdapter();
-//            Log.d("MOJAR", "Listview!");
-//            if(adapter != null){
-//                int count = adapter.getCount();
-//                int mesured_height = listView.getMeasuredHeight();
-//                height = mesured_height;//FIXME костыль! не знаю как правильно обработать вычисление высоты
-//
-//            }
-//        }
-//
-//        Log.d("MOJAR", "Layout height: "+height);
+        int height = 0;
 
-        heightMeasureSpec = MeasureSpec.makeMeasureSpec(1024, MeasureSpec.EXACTLY);
+        mCurrentView.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+        int h = mCurrentView.getMeasuredHeight();
+        if(h > height) height = h;
+
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
